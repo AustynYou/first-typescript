c203393b2d3ed6bca0be46041c390bc74d73f010
AustynYou <austyn.you@gmail.com>
2022-05-06 23:33:35 +0900

    ch:07 How to Create a Function in TS
    Return Type of Function, Optional Parameters, Default Parameters

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-var sendGreeting;
-(function (message, userName) {
+var sendGreeting = function (message, userName) {
     if (message === void 0) { message = 'Hello'; }
     if (userName === void 0) { userName = 'there'; }
     return console.log("".concat(message, ", ").concat(userName));
-});
+};
 sendGreeting('Hello');
 sendGreeting();
 sendGreeting('Good Morning');
--- app.ts
+++ app.ts
@@ -1,9 +1,7 @@
-const sendGreeting (message = 'Hello', userName = 'there'): 
-void => 
-  console.log(`${message}, ${userName}`);
+const sendGreeting = (message = 'Hello', userName = 'there'): 
+void =>  console.log(`${message}, ${userName}`);
 
 sendGreeting('Hello');
-
 sendGreeting();
 sendGreeting('Good Morning');
 sendGreeting('Good afternoon', 'Jenny');
