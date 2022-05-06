06cb81eb331a80c3c0b9f1930491e1654994398c
AustynYou <austyn.you@gmail.com>
2022-05-06 23:03:22 +0900

    ch06: Union Type & Type Guards
    Any, Union Type, Type Aliases, & Type Guards

--- app.js
+++ app.js
@@ -1,33 +1,10 @@
-var GenderType;
-(function (GenderType) {
-    GenderType["Male"] = "male";
-    GenderType["Female"] = "female";
-    GenderType["genderNeutral"] = "genderNeutral";
-})(GenderType || (GenderType = {}));
-var studentID = 12345;
-var studentName = 'Jenny Kim';
-var age = 21;
-var gender = 'female';
-var subject = 'JavaScript';
-var courseCompleted = false;
-var student1 = {
-    studentID: 121212,
-    studentName: 'Janet Jackson',
-    age: 30,
-    gender: GenderType.Female,
-    subject: 'Mongo DB',
-    courseCompleted: false
+var itemPrice;
+var setItemPrice = function (price) {
+    if (typeof price === 'string') {
+        itemPrice = 0;
+    }
+    else {
+        itemPrice = price;
+    }
 };
-function getStudentDetails(studentID) {
-    return {
-        studentID: 123456,
-        studentName: 'Mark Jacobs',
-        gender: 'female',
-        subject: 'Node JS',
-        courseCompleted: true
-    };
-}
-function saveStudentDetails(student) {
-    student.studentID = 11222;
-}
-saveStudentDetails(student1);
+setItemPrice(50);
--- app.ts
+++ app.ts
@@ -1,50 +1,12 @@
-enum GenderType {
-  Male = 'male',
-  Female = 'female',
-  genderNeutral = 'genderNeutral'
-}
+type StringOrNum = string | number;
+let itemPrice: number;
 
-let studentID:number = 12345;
-let studentName:string = 'Jenny Kim';
-let age = 21;
-let gender:string = 'female';
-let subject:string = 'JavaScript';
-let courseCompleted:boolean = false;
+const setItemPrice = (price: StringOrNum):void => {
+  if (typeof price === 'string') {
+    itemPrice = 0;
+  } else {
+  itemPrice = price;
+  }
+};
 
-let student1 = {
-  studentID: 121212,
-  studentName: 'Janet Jackson',
-  age: 30,
-  gender: GenderType.Female,
-  subject: 'Mongo DB',
-  courseCompleted: false
-}
-
-interface Student {
-  readonly studentID:number;
-  studentName:string;
-  age?:number;
-  gender: 'male' | 'female' | 'genderNeutral';
-  subject:string;
-  courseCompleted:boolean;
-  // addComment (comment:string): string;
-  addComment?: (comment:string) => string;
-
-}
-
-function getStudentDetails(studentID: number): Student 
-{
-  return {
-  studentID: 123456,
-  studentName: 'Mark Jacobs',
-  gender:'female',
-  subject: 'Node JS',
-  courseCompleted: true
-  };
-}
-
-function saveStudentDetails (student: Student): void {
-  student.studentID = 11222;
-}
-
-saveStudentDetails(student1)
+setItemPrice(50);
