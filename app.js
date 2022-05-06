34cf266c62db456207f78a6a13cd16578b32f7e5
AustynYou <austyn.you@gmail.com>
2022-05-06 21:55:28 +0900

    ch:04 Interface

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
 var studentID = 12345;
 var studentName = 'Jenny Kim';
-var gender = 'demale';
+var age = 21;
+var gender = 'female';
 var subject = 'JavaScript';
 var courseCompleted = false;
+var student1 = {
+    studentID: 121212,
+    studentName: 'Janet Jackson',
+    age: 30,
+    gender: 'Femail',
+    subject: 'Mongo DB',
+    courseCompleted: false
+};
+function getStudentDetails(studentID) {
+    return {
+        studentID: 123456,
+        studentName: 'Mark Jacobs',
+        gender: 'male',
+        subject: 'Node JS',
+        courseCompleted: true
+    };
+}
+function saveStudentDetails(student) {
+    student.studentID = 11222;
+}
+saveStudentDetails(student1);
--- app.ts
+++ app.ts
@@ -1,16 +1,44 @@
 let studentID:number = 12345;
 let studentName:string = 'Jenny Kim';
-let gender:string = 'demale';
+let age = 21;
+let gender:string = 'female';
 let subject:string = 'JavaScript';
 let courseCompleted:boolean = false;
 
-function getStudentDetails(studentID: number) 
-:{
-  studentID:number;
+let student1 = {
+  studentID: 121212,
+  studentName: 'Janet Jackson',
+  age: 30,
+  gender: 'Femail',
+  subject: 'Mongo DB',
+  courseCompleted: false
+}
+
+interface Student {
+  readonly studentID:number;
   studentName:string;
+  age?:number;
   gender:string;
   subject:string;
-  createDate: Date;
-} {
-  return null;
+  courseCompleted:boolean;
+  // addComment (comment:string): string;
+  addComment?: (comment:string) => string;
+
 }
+
+function getStudentDetails(studentID: number): Student 
+{
+  return {
+  studentID: 123456,
+  studentName: 'Mark Jacobs',
+  gender: 'male',
+  subject: 'Node JS',
+  courseCompleted: true
+  };
+}
+
+function saveStudentDetails (student: Student): void {
+  student.studentID = 11222;
+}
+
+saveStudentDetails(student1)
